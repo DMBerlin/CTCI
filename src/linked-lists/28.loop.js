afcdd15a275b6d7f4a6235c86642f544924b4193
Daniel Marinho <contato@danielmarinho.dev>
2022-06-20 23:15:27 -0300

    chore: 28 - loop

---
+++ src/linked-lists/28.loop.js
@@ -0,0 +1,83 @@
+/**
+ * Loop Detection: Given a circular linked list, implement an algorithm
+ * that returns the node at the beginning of the loop.
+ * DEFINITION
+ * Circular linked list: A (corrupt) linked list in which a node's next
+ * pointer points to an earlier node, so as to make a loop in the linked list.
+ * EXAMPLE
+ * Input: A -> B -> C - > D -> E -> C [the same C as earlier]
+ * Output: C
+ */
+
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor () {
+        this.head = null;
+    }
+
+    add (data) {
+        this.head = new Node(data, this.head);
+    }
+
+    push (node) {
+        node.next = this.head;
+        this.head = node;
+    }
+
+    build (nodes) {
+        nodes.forEach((node) => {
+            this.push(node);
+        });
+    }
+
+    contains (ref) {
+        let node = this.head;
+        let counter = 0;
+        while (node) {
+            if (node === ref) {
+                counter++;
+                if (counter > 1) {
+                    return counter;
+                }
+            }
+            node = node.next;
+        }
+        return counter;
+    }
+}
+
+
+const node5 = new Node(5);
+const linkedList = new LinkedList();
+
+linkedList.build([
+    new Node(1),
+    new Node(2),
+    new Node(3),
+    new Node(4),
+    node5,
+    new Node(6),
+    new Node(7),
+    new Node(8),
+    new Node(9),
+    node5,
+]);
+
+const loopable = (list) => {
+    let node = list.head;
+    while (node) {        
+        if (list.contains(node) > 1) {
+            return node;
+        }
+        node = node.next;
+    }
+    return false;
+}
+
+console.log(loopable(linkedList));
